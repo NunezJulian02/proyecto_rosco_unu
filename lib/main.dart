7a43d9c0ddba01b8756529a7b650e5178d183389
Nuñez Julian <julian.nunezescobar02@gmail.com>
2021-06-23 12:53:56 -0300

    Agregados, videos 11, 12, 13 y 22

--- lib/main.dart
+++ lib/main.dart
@@ -2,72 +2,20 @@
 
 // {} Creando El juego Rosco (Pasapalabra)
 import 'dart:html';
-
-void main() {
-  //realizando una instancia de clase
-  var rosco = Rosco();
-  //Llamando a un metodo
-  var primeraDefinicion = rosco.obtenerPregunta();
-  //Conectando primeradefinicion con el Label Html
-  //Con solo el ID del label html se puede modificar su valor
-  querySelector("#pregunta").text = primeraDefinicion.definicion;
-  querySelector("#letra").text = primeraDefinicion.letra;
-
-  //Captura el evento de una funcion
-  querySelector("#btnEnviar").onClick.listen((event) {
-    //Vamos a tomar los valores de respuesta y letra para almacenarlos
-    //V convierte el queryS. a un input (el de html) para tomar su valor
-    var respuesta = (querySelector("#textRespuesta") as InputElement).value;
-    //Ya tomamos el valor de respuesta, ahora va el de letra
-    var letra = querySelector("#letra").text;
-
-    //LLamando al metodo evaluarrespuesta
-    String mensaje = rosco.evaluarRespuesta(letra, respuesta);
-
-    print(mensaje);
-  });
-} //Metodo
+import 'dart:collection';
 
 class Rosco {
   //Creando una lista del Tipo Pregunta (Char, int, etc)
-  List<Pregunta> roscoPreguntas = [];
-  //Datos del Juego (Atributos)
-  //Las variables son temporales
-  //Static = Indica que la variable permanece en la memoria
-  //const = indica que los valores no van a cambiar
-  static List letras = const ["A", "B", "C", "D", "E", "F"];
-  static List definiciones = const [
-    "Persona que tripula una Astronave o que estรก entrenada para este Trabajo",
-    "Especie de Talega o Saco de Tela y otro material que sirve para llevar o guardar algo",
-    "Aparato destinado a registrar imรกgenes animadas para el cine o la telivision",
-    "Obra literaria escrita para ser representada",
-    "Que se prolonga muchisimo o excesivamente",
-    "Laboratorio y despacho del farmaceutico"
-  ];
-  static List respuestas = [
-    "Astronauta",
-    "Bolsa",
-    "Camara",
-    "Drama",
-    "Eterno",
-    "Farmacia"
-  ];
+  ListQueue<Pregunta> roscoPreguntas =
+      ListQueue<Pregunta>(); //inicializando lista
   //Creando el Constructor para Cargar las preguntas con valores
   Rosco() {
-    //El for recorre la primera coleccion de letras
-    //el index toma los valores de las colecciones def. y respuestas
-    for (var letra in letras) {
-      var index = letras.indexOf(letra);
-      //Se crea una instancia de pregunta, envia las letras cargadas
-      //en los atributos y los almacena en la lista "roscoPreguntas"
-      var roscoPregunta =
-          Pregunta(letra, definiciones[index], respuestas[index]);
-      roscoPreguntas.add(roscoPregunta);
-    }
+    roscoPreguntas.addAll(RoscoApi().obtenerRoscos());
   }
   //Creando el Retorno del metodo
-  Pregunta obtenerPregunta() {
-    return roscoPreguntas[0];
+  Pregunta obtenerPregunta(bool inicial) {
+    if (inicial) return roscoPreguntas.first;
+    return null;
   }
 
   //Metodo
@@ -79,14 +27,12 @@ class Rosco {
   String evaluarRespuesta(String letra, String respuesta) {
     //almacena la variable boolean de any
     //any busca variables iguales dentro de roscoPreguntas
-    var esCorrecta = roscoPreguntas.any(
-        (funcion) => funcion.letra == letra && funcion.respuesta == respuesta);
+    var esCorrecta = roscoPreguntas
+        .any((rosco) => rosco.letra == letra && rosco.respuesta == respuesta);
 
-    if (esCorrecta == true) {
-      return "letra $letra respuesta correcta";
-    }
-
-    return "Letra $letra respuesta incorrecta";
+    return esCorrecta
+        ? "Letra $letra respuesta correcta"
+        : "Letra $letra respuesta incorrecta"; //Otra forma de IF
   }
 }
 
@@ -102,3 +48,72 @@ class Pregunta {
     this.respuesta,
   );
 }
+
+class Db {
+  //Datos del Juego (Atributos)
+  //Las variables son temporales
+  //Static = Indica que la variable permanece en la memoria
+  //const = indica que los valores no van a cambiar
+  static List letras = const ["A", "B", "C", "D", "E", "F"];
+  static List definiciones = const [
+    "Persona que tripula una Astronave o que estaก entrenada para este Trabajo",
+    "Especie de Talega o Saco de Tela y otro material que sirve para llevar o guardar algo",
+    "Aparato destinado a registrar imรกgenes animadas para el cine o la telivision",
+    "Obra literaria escrita para ser representada",
+    "Que se prolonga muchisimo o excesivamente",
+    "Laboratorio y despacho del farmaceutico"
+  ];
+  static List respuestas = [
+    "Astronauta",
+    "Bolsa",
+    "Camara",
+    "Drama",
+    "Eterno",
+    "Farmacia"
+  ];
+}
+
+class RoscoApi {
+  List<Pregunta> roscoPreguntas = [];
+
+  List<Pregunta> obtenerRoscos() {
+    //El for recorre la primera coleccion de letras
+    //el index toma los valores de las colecciones def. y respuestas
+    for (var letra in Db.letras) {
+      var index = Db.letras.indexOf(letra);
+      //Se crea una instancia de pregunta, envia las letras cargadas
+      //en los atributos y los almacena en la lista "roscoPreguntas"
+      var roscoPregunta =
+          Pregunta(letra, Db.definiciones[index], Db.respuestas[index]);
+      roscoPreguntas.add(roscoPregunta);
+      //Cuando un atributo es "static" no necesita ser instanciado.
+    }
+
+    return roscoPreguntas;
+  }
+}
+
+void main() {
+  //Nos comunica con el HTML y demas clases
+  //realizando una instancia de clase
+  var rosco = Rosco();
+  //Llamando a un metodo
+  var primeraDefinicion = rosco.obtenerPregunta(true);
+  //Conectando primeradefinicion con el Label Html
+  //Con solo el ID del label html se puede modificar su valor
+  querySelector("#pregunta").text = primeraDefinicion.definicion;
+  querySelector("#letra").text = primeraDefinicion.letra;
+
+  //Captura el evento de una funcion
+  querySelector("#btnEnviar").onClick.listen((event) {
+    //Vamos a tomar los valores de respuesta y letra para almacenarlos
+    //V convierte el queryS. a un input (el de html) para tomar su valor
+    var respuesta = (querySelector("#textRespuesta") as InputElement).value;
+    //Ya tomamos el valor de respuesta, ahora va el de letra
+    var letra = querySelector("#letra").text;
+
+    //LLamando al metodo evaluarrespuesta
+    String mensaje = rosco.evaluarRespuesta(letra, respuesta);
+    print(mensaje);
+  });
+} //Metodo
